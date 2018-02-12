1a31a328df016e5472f1769bb45c030330f0f927
DimaKhaver\Dima <xaverdima@gmail.com>
2018-02-12 17:20:24 +0200

    Message passing using the Binder - one way communication

---
+++ app/src/main/java/com/example/dima/androidmultithreading/interprocess_communication/ISynchronous.kt
@@ -0,0 +1,7 @@
+package com.example.dima.androidmultithreading.interprocess_communication
+
+/**
+ * Created by Dima on 2/10/2018.
+ */
+interface ISynchronous {
+}
---
+++ app/src/main/java/com/example/dima/androidmultithreading/interprocess_communication/one_way_communication/MessengerOneWayActivity.kt
@@ -0,0 +1,7 @@
+package com.example.dima.androidmultithreading.interprocess_communication.one_way_communication
+
+/**
+ * Created by Dima on 2/12/2018.
+ */
+class MessengerOnewayActivity {
+}
